335d3b92bd5fd8cc47ee4bbbd73c9674951cc67e
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2020-11-16 13:41:39 -0800

    added base case

--- src/HiLoStress.java
+++ src/HiLoStress.java
@@ -3,38 +3,39 @@ import java.util.*;
 import java.io.*;
 
 public class HiLoStress {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		final String FILENAME = "jobs.txt";
 		File text = new File(FILENAME);
 
 		Scanner s = new Scanner(text);
 		int n = s.nextInt();
-		int[] high = new int[n];
-		int[] low = new int[n];
-		for (int i = 0; i<n ; i++) {
+		int[] high = new int[n+1];
+		int[] low = new int[n+1];
+		for (int i = 1; i<=n ; i++) {
 			low[i] = s.nextInt();
 			high[i] = s.nextInt();
 		}
 		s.close();
 		
-		
-		int size = 5;
-		int[] low = new int[size];
-		int[] high = new int[size];
-		int[] payouts = new int[size+1];
-		
-		payouts[0] = high[0];
-		payouts[1] = low[0]+high[1];
-		for(int i = 2; i < size; i++) {
-			int highRoute = high[i]+low[i-1]+payouts[i-2];
-			int lowRoute = low[i]+payouts[i-1];
-			if(highRoute>lowRoute) {
+
+		int[] payouts = new int[n+1];
+		//first two days
+		if (high[2] > low[1] + low[2]) {
+			payouts[1] = 0;
+			payouts[2] = high[2];
+		} else {
+			payouts[1] = low[1];
+			payouts[2] = low[1] + low[2];
+		}
+		for (int i=2; i<=n; i++) {
+			int highRoute = high[i] + payouts[i-2];
+			int lowRoute = low[i] + payouts[i-1];
+			if (highRoute>lowRoute) {
 				payouts[i] = highRoute;
-			}
-			else {
+			} else {
 				payouts[i] = lowRoute;
 			}
 		}
-		System.out.println(payouts[size-1]);
+		System.out.println(payouts[n]);
 	}
 }

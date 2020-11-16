27769c88039915aaa3d44a6e9b0b7ab7cc01c12d
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2020-11-16 11:12:38 -0800

    read the file into high and low arrays

--- src/HiLoStress.java
+++ src/HiLoStress.java
@@ -1,4 +1,19 @@
 
-public class HiLoStress {
+import java.util.*;
+import java.io.*;
 
+public class HiLoStress {
+		final String FILENAME = "jobs.txt";
+		File text = new File(FILENAME);
+
+		Scanner s = new Scanner(text);
+		int n = s.nextInt();
+		int[] high = new int[n];
+		int[] low = new int[n];
+		for (int i = 0; i<n ; i++) {
+			low[i] = s.nextInt();
+			high[i] = s.nextInt();
+		}
+		s.close();
+	}
 }
